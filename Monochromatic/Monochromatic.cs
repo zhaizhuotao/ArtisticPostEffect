c2ceae87bb477dc6fa292efb67efce4384873563
zhaizhuotao <zhaizhuotao@me.com>
2018-08-14 15:00:20 +0800

    添加单色效果
    添加单色效果

---
+++ Monochromatic/Monochromatic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monochromatic : MonoBehaviour {
+    public Shader MonochromaticShader;
+    private Material _monochromaticMat;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (_monochromaticMat == null)
+        {
+            _monochromaticMat = new Material(MonochromaticShader);
+        }
+        Graphics.Blit(source,destination,_monochromaticMat);
+    }
+}
---
+++ VideoCapturePlane.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoCapturePlane : MonoBehaviour {
+    public int CaptureWidth = 640;
+    public int CaptureHeight = 480;
+    public WebCamTexture m_webcamTexture = null;
+    public Material VideoCapturePlaneMat;
+    private string deviceName;
+	// Use this for initialization
+	void Start () {
+        for (int cameraIndex = 0; cameraIndex < WebCamTexture.devices.Length; cameraIndex++)
+        {
+            // We want the back camera
+            if (!WebCamTexture.devices[cameraIndex].isFrontFacing)
+            {
+                deviceName = WebCamTexture.devices[cameraIndex].name;
+                m_webcamTexture = new WebCamTexture(deviceName, CaptureWidth, CaptureHeight, 30);
+                break;
+            }
+            else if (WebCamTexture.devices[cameraIndex].isFrontFacing)
+            {
+                deviceName = WebCamTexture.devices[cameraIndex].name;
+                m_webcamTexture = new WebCamTexture(deviceName, CaptureWidth, CaptureHeight, 30);
+                break;
+            }
+        }
+        VideoCapturePlaneMat.mainTexture = m_webcamTexture;
+        m_webcamTexture.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
